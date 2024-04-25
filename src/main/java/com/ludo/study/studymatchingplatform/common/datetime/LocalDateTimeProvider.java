82e8ebb2ef0b9ea45724e07fc501bc528a1ebfa4
june-777 <wlwhswnsrl96@gmail.com>
2024-04-25 18:08:32 +0900

    feat: JPA Auditing ms 단위로 통일
    - Linux Ubuntu 환경에서 ns 단위까지 기록되는 현상 발생

---
+++ src/main/java/com/ludo/study/studymatchingplatform/common/datetime/LocalDateTimeProvider.java
@@ -0,0 +1,17 @@
+package com.ludo.study.studymatchingplatform.common.datetime;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAccessor;
+import java.util.Optional;
+
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LocalDateTimeProvider implements DateTimeProvider {
+	@Override
+	public Optional<TemporalAccessor> getNow() {
+		return Optional.of(LocalDateTime.now().truncatedTo(ChronoUnit.MICROS));
+	}
+}
