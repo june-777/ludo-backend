cdb9c439e3e7e673de016143e22e0d933637cd42
june-777 <wlwhswnsrl96@gmail.com>
2024-04-30 21:08:21 +0900

    refactor: 정확한 원인 분석을 위해 JpaAuditing ms 단위로 통일 로직을 제거
    - 날짜로 인해 테스트 실패했던 원인이 JpaAuditing 인지, Fixture 로직도 포함인지 확인하기 위함

--- src/main/java/com/ludo/study/studymatchingplatform/common/datetime/LocalDateTimeProvider.java
+++ src/main/java/com/ludo/study/studymatchingplatform/common/datetime/LocalDateTimeProvider.java
@@ -9,9 +9,9 @@ import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.stereotype.Component;
 
 @Component
-public class LocalDateTimeProvider implements DateTimeProvider {
-	@Override
-	public Optional<TemporalAccessor> getNow() {
-		return Optional.of(LocalDateTime.now().truncatedTo(ChronoUnit.MICROS));
-	}
+public class LocalDateTimeProvider {
+	// @Override
+	// public Optional<TemporalAccessor> getNow() {
+	// 	return Optional.of(LocalDateTime.now().truncatedTo(ChronoUnit.MICROS));
+	// }
 }
